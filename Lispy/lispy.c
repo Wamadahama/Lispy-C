7ead470e91522a2b2c63262ba34c664730332ae5
Wamadahama <smurftown2@gmail.com>
2016-01-30 22:09:13 -0500

    Chapter 6

--- Lispy/lispy.c
+++ Lispy/lispy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -9,7 +10,7 @@ static char buffer[2048];
 char* readline(char* prompt){
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = mallock(strlen(buffer) + 1)
+  char* cpy = malloc(strlen(buffer) + 1);
   strcpy(cpy, buffer);
   cpy[strlen(cpy)-1] = '\0';
   return cpy;
@@ -23,9 +24,24 @@ void add_history(char* unused){}
 #include <editline/history.h>
 #endif
 /* Declare a buffer for the user input of size 2048 */
-static char input[2048];
 
 int main(void){
+
+  /* Create the parsers for mathematical expressions */
+  mpc_parser_t* Number    = mpc_new("number");
+  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Expr      = mpc_new("expr");
+  mpc_parser_t* Lispy     = mpc_new("lispy");
+
+  /* Define the grammas in the language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                           \
+    number   : /-?[0-9]+/ ;                                   \
+    operator : '+' | '-' | '*' | '/' | '\%' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;        \
+    lispy    : /^/ <operator> <expr>+ /$/  ;                  \
+  ",
+  Number, Operator, Expr, Lispy);
 
   /* Print version information*/
   puts("Lispy Version 0.0.0.0.1");
@@ -38,10 +54,22 @@ int main(void){
       char* input = readline("E-Lispy> ");
       add_history(input);
 
-      /* Eacho input back to the user*/
-      printf("No you're a %s", input);
+      /* Echo input back to the user*/
+      mpc_result_t r;
+      if (mpc_parse("<stdin>", input, Lispy, &r)) {
+        /* On Success Print the AST */
+        mpc_ast_print(r.output);
+        mpc_ast_delete(r.output);
+      } else {
+        /* Otherwise Print the error */
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+      }
+
+
       free(input);
   }
 
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
